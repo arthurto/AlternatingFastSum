83a953f4618ab797e7f16bd9cdb0ed5194ef7813
arthurto <arthur.ps22009@gmail.com>
2023-11-20 17:32:32 -0300

    first commit

---
+++ convergence.jl
@@ -0,0 +1,25 @@
+# This program will going to be used to calculate
+# the values of some numerical alternating series 
+
+# Using the algorithm from [1]
+# Refs: 
+# 
+# [1] - Cohen, H., Villegas, F.R. and Zagier, D., 
+# 2000. Convergence acceleration of alternating series. 
+# Experimental mathematics, 9(1), pp.3-12.
+
+function sum_alt(n::Int64,a::Function)
+    # Initializing parameters
+    d = (3.0 + √8)^n
+    d = (d + 1/d)/2 
+    b = -1.0; c = -d; s = 0.0
+
+    # Performinf the algorithm 
+    for k in 0:(n-1)
+        c = b - c; s += c*a(k);
+        b = (k + n)*(k-n)*b/((k+1/2)*(k+1));
+    end
+    
+    # Returning the value 
+    return s/d
+end
