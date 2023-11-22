36aa4819f28cda78376bf2b38ef78d70df0cb8ac
arthurto <arthur.ps22009@gmail.com>
2023-11-22 09:12:44 -0300

    new tests

--- examples.jl
+++ examples.jl
@@ -1,3 +1,5 @@
+include("convergence.jl")
+
 begin 
     function ak_ln1x(x,k)
         return x^(k+1)/(k+1)
@@ -8,16 +10,22 @@ begin
     end
 end
 
-begin 
-    # the log(1+x) alternating sum 
-    sum_alt(7,(k->ak_ln1x(1.0,k)))-log(2)
-    sum([(-1)^k*ak_ln1x(1.0,k) for k in 0:1_000_000])-log(2)
-    # For the second class of algoritms we must be able
-    # to calculate the polynomial 
+
+# the log(1+x) alternating sum 
+sum_alt(7,(k->ak_ln1x(1.0,k)))-log(2)
+sum([(-1)^k*ak_ln1x(1.0,k) for k in 0:1_000_000])-log(2)
+# For the second class of algoritms we must be able
+# to calculate the polynomial 
 
 
-    # for the e^(-n^2) sum 
-    sum_alt(15,(k->f(1.0,k)))-0.386318602413326
-    sum([(-1)^k*f(1.0,k) for k in 0:1_000_000])-0.386318602413326
-    1/(exp(1)-1)
-end
+# for the e^(-n^2) sum
+# we also have this improved performance  
+sum_alt(30,(k->f(1.0,k)))-0.386318602413326
+sum([(-1)^k*f(1.0,k) for k in 0:1_000_000])-0.386318602413326
+1/(exp(1)-1)
+
+
+# for the e^(-n) sum 
+# we dont...
+sum_alt(100,(k->exp(-k)*(-1)^k))-exp(1)/(exp(1)-1)
+sum([exp(-k) for k in 0:100])-exp(1)/(exp(1)-1)
